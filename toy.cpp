a44a70f19ebec80a8cc5bffc35a3b93e0d792287
Omer Osman <omer.m.o.hussain@gmail.com>
2024-06-28 09:35:48 +0100

    Implement token handlers and driver code. Lexer, Parser, and AST builder now complete and validate code against grammar definition

--- toy.cpp
+++ toy.cpp
@@ -394,4 +394,75 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
 }
 
 
+// Top level parsing handlers 
 
+static void HandleDefinition() {
+	if (ParseDefinition()) {
+		fprintf(stderr, "Parsed a function definition.\n");
+	} else {
+		// Skip token for error recovery 
+		getNextToken();
+	}
+}
+
+
+static void HandleExtern() {
+	if (ParseExtern()) {
+		fprintf(stderr, "Parsed an extern\n");
+	} else {
+		// Skip token for error recovery
+		getNextToken();
+	}
+}
+
+static void HandleTopLevelExpression() {
+	// Evaluate a top level expression into an anonymous function 
+	if (ParseTopLevelExpr()) {
+		fprintf(stderr, "Parsed a top level expression\n");
+	} else {
+		// Skip token for error recover
+		getNextToken();
+	}
+}
+
+// top ::= definition | external | expression | ';'
+// Ignore top level semicolons for user flexibility
+static void MainLoop() {
+  while (true) {
+    fprintf(stderr, "ready> ");
+    switch (CurTok) {
+    case tok_eof:
+      return;
+    case ';': // ignore top-level semicolons.
+      getNextToken();
+      break;
+    case tok_def:
+      HandleDefinition();
+      break;
+    case tok_extern:
+      HandleExtern();
+      break;
+    default:
+      HandleTopLevelExpression();
+      break;
+    }
+  }
+}
+
+int main() {
+  // Install standard binary operators.
+  // 1 is lowest precedence.
+  BinopPrecedence['<'] = 10;
+  BinopPrecedence['+'] = 20;
+  BinopPrecedence['-'] = 20;
+  BinopPrecedence['*'] = 40; // highest.
+
+  // Prime the first token.
+  fprintf(stderr, "ready> ");
+  getNextToken();
+
+  // Run the main "interpreter loop" now.
+  MainLoop();
+
+  return 0;
+}
