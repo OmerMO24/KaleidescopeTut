2cd4b9c495795d1e5c1de0b38c3b36dc17f0d8a5
Omer Osman <omer.m.o.hussain@gmail.com>
2024-06-28 09:10:47 +0100

    Implement parsers for function prototypes, function bodies and the 'extern' keyword.

--- toy.cpp
+++ toy.cpp
@@ -334,3 +334,64 @@ static std::unique_ptr<ExprAST> ParseExpression() {
 	
 	return ParseBinOpRHS(0, std::move(LHS));
 }
+
+// prototype 
+//	::= id '(' id* ')'
+static std::unique_ptr<PrototypeAST> ParsePrototype() {
+	if (CurTok != tok_identifier)
+		return LogErrorP("Expected function name in prototype");
+
+	std::string FnName = IdentifierStr; 
+	getNextToken();
+
+	if (CurTok != '(')
+		return LogErrorP("Expected '(' in prototype");
+
+	// Read the list of argument names 
+	std::vector<std::string> ArgNames;
+	while (getNextToken() == tok_identifier)
+		ArgNames.push_back(IdentifierStr);
+	if(CurTok != ')')
+		return LogErrorP("Expected ')' in prototype");
+
+	// success
+	getNextToken(); // eat ')'
+	
+	return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+}
+
+
+// definition ::= 'def' prototype expression
+// This is just the function prototype plus an expression to implement the body 
+// So functions are essentially composed of a prototype plus a body expression 
+static std::unique_ptr<FunctionAST> ParseDefinition() {
+	getNextToken(); // eat 'def'
+	auto Proto = ParsePrototype();
+	if (!Proto) return nullptr;
+
+	if (auto E = ParseExpression())
+		return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+	return nullptr;
+}
+
+
+// external ::= 'extern' prototype 
+// extern is used similarly as in C/C++, declares golobal variable or function that is defined in another file or library 
+// No memory allocated by extern
+static std::unique_ptr<PrototypeAST> ParseExtern() {
+	getNextToken(); // eat extern
+	return ParsePrototype();
+}
+
+// toplevelexpr ::= expression 
+static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
+	if (auto E = ParseExpression()) {
+		// Make an anonymous proto
+		auto Proto = std::make_unique<PrototypeAST>("", std::vector<std::string>());
+		return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+	}
+	return nullptr;
+}
+
+
+
