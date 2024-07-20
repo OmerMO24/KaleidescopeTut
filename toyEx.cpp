5c00762a07ea821fe4e11a429054f0cdf65e8308
Omer Osman <omer.m.o.hussain@gmail.com>
2024-07-20 22:52:30 +0400

    Implement properly scoped variable definitions

--- toyEx.cpp
+++ toyEx.cpp
@@ -988,6 +988,57 @@ Value *ForExprAST::codegen() {
 
 
 
+Value *VarExprAST::codegen() {
+
+	std::vector<AllocaInst *> OldBindings;
+
+	Function *TheFunction = Builder->GetInsertBlock()->getParent();
+
+	// Register all variables and emit their initializer
+	for (unsigned i = 0, e = VarNames.size(); i != e; ++i) {
+		const std::string &VarName = VarNames[i].first;
+		ExprAST *Init = VarNames[i].second.get();
+
+		// Emit the initializer befgore adding the variable to scope, this prevents 
+		// the intializer from referencing the variable itself, and permits stuff like this 
+		//  var a = 1 in
+		//		var a = a in .... #refers to outer 'a'
+		Value *InitVal;
+		if (Init) {
+			InitVal = Init->codegen();
+			if (!InitVal)
+				return nullptr;
+		} else { // if not specified, use 0.0 
+			InitVal = ConstantFP::get(*TheContext, APFloat(0.0));
+		}
+
+		AllocaInst *Alloca = CreateEntryBlockAlloca(TheFunction, VarName);
+		Builder->CreateStore(InitVal, Alloca);
+
+
+		// Remember the old variable binding so that we can restore the binding when we unrecurse
+		OldBindings.push_back(NamedValues[VarName]);
+
+
+		// Remember this binding 
+		NamedValues[VarName] = Alloca;
+	}
+	
+	// Codegen the body, now that all the vars are in scope 
+	Value *BodyVal = Body->codegen();
+	if (!BodyVal)
+		return nullptr;
+
+	// Pop all fo our variables from scope
+	for (unsigned i = 0, e = VarNames.size(); i != e; ++i)
+		NamedValues[VarNames[i].first] = OldBindings [i];
+
+	// Return the body computation 
+	return BodyVal;
+}
+
+
+
 Function *PrototypeAST::codegen() {
   // Make the function type:  double(double,double) etc.
   std::vector<Type *> Doubles(Args.size(), Type::getDoubleTy(*TheContext));
